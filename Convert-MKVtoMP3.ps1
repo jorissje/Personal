7b34351a776f51c0adbca8c04221184fa9e36e87
jorissje <joris@famvanderhelm.nl>
2021-05-03 18:51:03 +0200

    Update Convert-MKVtoMP3.ps1

--- Convert-MKVtoMP3.ps1
+++ Convert-MKVtoMP3.ps1
@@ -1,9 +1,13 @@
 #Created by Joris
 #Date : 03-05-2021
+#Requires FFMPEG (Windows Binary)
+#https://github.com/FFmpeg/FFmpeg
+############################################################
+
+#Input Variables#
 $path = 'G:\My Drive\Media\Muziek\Supersized Kingsday (2021)'
 
 ############################################################
-
 
 $mkvfiles = Get-ChildItem ($path + '\*') -Include *.mkv
 
@@ -12,3 +16,4 @@ foreach ($file in $mkvfiles)
 $shortfile = ($file.name -replace ".{4}$", ".mp3")
 ffmpeg -i $file.Name -vn -c:a libmp3lame -y $shortfile #-enable-libmp3lame
 }
+
